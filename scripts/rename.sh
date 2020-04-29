9cea98bd19f1f7ec3075b11677a02d55264831ee
Sheldon Johnson <shayolden@hotmail.com>
2020-04-30 01:38:38 +1000

    Stash scripts in repo for now (grename renamer)

---
+++ scripts/rename.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mv fortress/textures/models/grenade.png_0 fortress/textures/models/grenade.mdl_0
+mv fortress/textures/models/grenade2.png_4 fortress/textures/models/grenade2.mdl_4
+mv fortress/textures/models/grenade2.png_2 fortress/textures/models/grenade2.mdl_2
+mv fortress/textures/models/grenade2.png_1 fortress/textures/models/grenade2.mdl_1
+mv fortress/textures/models/grenade2.png_3 fortress/textures/models/grenade2.mdl_3
+mv fortress/textures/models/grenade2.png_0 fortress/textures/models/grenade2.mdl_0
+mv fortress/textures/models/grenade_0.png_0 fortress/textures/models/grenade_0.mdl_0
+mv fortress/textures/models/grenade_luma.png_0 fortress/textures/models/grenade_luma.mdl_0
+mv fortress/textures/models/grenade2_luma.png_3 fortress/textures/models/grenade2_luma.mdl_3
+mv fortress/textures/models/grenade2_luma.png_0 fortress/textures/models/grenade2_luma.mdl_0
+mv fortress/textures/models/grenade2_luma.png_2 fortress/textures/models/grenade2_luma.mdl_2
+mv fortress/textures/models/grenade2_luma.png_4 fortress/textures/models/grenade2_luma.mdl_4
+mv fortress/textures/models/grenade2_luma.png_1 fortress/textures/models/grenade2_luma.mdl_1
