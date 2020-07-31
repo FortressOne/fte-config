963fae7dfcd2ac79ddbb1ae5450a583154c2a409
Sheldon Johnson <shayolden@hotmail.com>
2020-07-31 22:50:32 +1000

    Don't use debug

--- build.sh
+++ build.sh
@@ -3,19 +3,23 @@
 version=$(cat VERSION)
 
 mkdir -p output/
+mkdir -p tmp/FortressOne/id1/
 mkdir -p tmp/windows/FortressOne/
 mkdir -p tmp/linux/FortressOne/
 
-curl http://triptohell.info/moodles/win64/debug/fteqw64.exe \
+curl https://www.mirafiori.com/ftp/pub/gaming/pak0.pak \
+  --output tmp/FortressOne/id1/pak0.pak
+
+curl http://triptohell.info/moodles/win64/fteqw64.exe \
   --output tmp/windows/FortressOne/fteqw64.exe
 
-curl http://triptohell.info/moodles/win64/debug/fteplug_ezhud_x64.dll \
+curl http://triptohell.info/moodles/win64/fteplug_ezhud_x64.dll \
   --output tmp/windows/FortressOne/fteplug_ezhud_x64.dll
 
-curl http://triptohell.info/moodles/linux_amd64/debug/fteqw64 \
+curl http://triptohell.info/moodles/linux_amd64/fteqw64 \
   --output tmp/linux/FortressOne/fteqw64
 
-curl http://triptohell.info/moodles/linux_amd64/debug/fteplug_ezhud_amd64.so \
+curl http://triptohell.info/moodles/linux_amd64/fteplug_ezhud_amd64.so \
   --output tmp/linux/FortressOne/fteplug_ezhud_amd64.so
 
 git archive \
@@ -24,6 +28,10 @@ git archive \
   --output git.zip \
   HEAD \
   FortressOne/
+
+cd tmp/ || exit
+zip -ur ../git.zip FortressOne/
+cd ../
 
 cp git.zip tmp/windows/fortressone-fte-windows-${version}-portable.zip
 mv git.zip tmp/linux/fortressone-fte-linux-${version}-portable.zip
