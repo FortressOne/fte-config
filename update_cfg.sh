2fe8f8b7dd8c926eee33bc9283e838ede93fe85d
Sheldon Johnson <shayolden@hotmail.com>
2021-07-19 00:32:15 +1000

    Fix update_cfg script

--- update_cfg.sh
+++ update_cfg.sh
@@ -1,6 +1,6 @@
 cp FortressOne/fortress/default.cfg FortressOne/fortress/fte.cfg
-FortressOne/fteqw64 -basedir FortressOne/ +exec update_cfg.cfg +cfg_save +quit
+FortressOne/fortressone64 -basedir FortressOne/ +exec update_cfg.cfg +cfg_save +quit
 cp FortressOne/fortress/fte.cfg FortressOne/fortress/default.cfg
-FortressOne/fteqw64 -basedir FortressOne/ +exec update_cfg.cfg +cfg_save +quit
+FortressOne/fortressone64 -basedir FortressOne/ +exec update_cfg.cfg +cfg_save +quit
 cp FortressOne/fortress/fte.cfg FortressOne/fortress/default.cfg
 git diff
